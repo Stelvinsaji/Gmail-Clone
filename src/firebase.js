f284ddb5adf18661bc113534cbbcd4d2b2836db7
Stelvinsaji <87966154+Stelvinsaji@users.noreply.github.com>
2021-12-05 12:20:41 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrOE-UliM76kOl2Y5FAamhyIMrw_x-VTI",
-  authDomain: "clone-ae56a.firebaseapp.com",
-  projectId: "clone-ae56a",
-  storageBucket: "clone-ae56a.appspot.com",
-  messagingSenderId: "457304453961",
-  appId: "1:457304453961:web:de723b409e755a5b016098"
+  // Paste ur Firebase Config here
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
